7d8d5cc401756cf7cab5f888b07fdddd0e727a3e
osaka <hallonavi@gmail.com>
2015-06-03 16:53:13 -0500

    TestSuite added to git.

---
+++ TestSuite/stdafx.h
@@ -0,0 +1,16 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+//Not needed because I added stdafx.h from OsakaEngine project
+//#include <stdio.h>
+//#include <tchar.h>
+
+
+
+// TODO: reference additional headers your program requires here
