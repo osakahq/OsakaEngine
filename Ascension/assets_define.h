9e03b321295416155998c9aa5bc75d6cc7581c35
osaka <hallonavi@gmail.com>
2015-06-07 17:37:16 -0500

    Minor change.

--- Ascension/assets_define.h
+++ Ascension/assets_define.h
@@ -3,14 +3,14 @@
 #ifndef ASC_ASSETSDEFINE_H
 #define ASC_ASSETSDEFINE_H
 
-#define SPRITE_STARTMENU_BACKGROUND "startmenu_background"
-#define SPRITE_STARTMENU_SUBTITLE	"startmenu_subtitle"
-#define SPRITE_STARTMENU_TITLE		"startmenu_title"
+#define ASC_SPRITE_STARTMENU_BACKGROUND "startmenu_background"
+#define ASC_SPRITE_STARTMENU_SUBTITLE	"startmenu_subtitle"
+#define ASC_SPRITE_STARTMENU_TITLE		"startmenu_title"
 
-#define SOUND_BGM_STARTMENU			"s_startmenu"
-#define SOUND_EFFECT_CURSOR_MOVE	"s_cursor_move"
-#define SOUND_EFFECT_CURSOR_CANCEL	"s_cursor_cancel"
+#define ASC_SOUND_BGM_STARTMENU			"s_startmenu"
+#define ASC_SOUND_EFFECT_CURSOR_MOVE	"s_cursor_move"
+#define ASC_SOUND_EFFECT_CURSOR_CANCEL	"s_cursor_cancel"
 
-#define SPRITE_TILE_WOOD_CENTER		"wood_center"
+#define ASC_SPRITE_TILE_WOOD_CENTER		"wood_center"
 
 #endif
