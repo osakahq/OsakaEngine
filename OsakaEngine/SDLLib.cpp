1ac2339230e55a977b5d256e083e9b96a2344697
osaka <hallonavi@gmail.com>
2015-06-11 19:08:05 -0500

    SDLLib: I changed the audio chunksize from 2048 to 1024, because it is so much more responsive. When playing a song, it takes 1ms instead of 2ms

--- OsakaEngine/SDLLib.cpp
+++ OsakaEngine/SDLLib.cpp
@@ -136,7 +136,7 @@ namespace Osaka{
 				}
 			}*/
 
-			if( Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0 ){
+			if( Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) < 0 ){
 				debug->l(std::string("[SDLLib] SDL_mixer could not initialize. SDL_mixer error: ") + Mix_GetError());
 				//return false;
 			}
